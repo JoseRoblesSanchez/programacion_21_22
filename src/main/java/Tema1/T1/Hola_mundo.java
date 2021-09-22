6c1dd686ff46bd7db0b3237bb38d15b3c4d0f614
José Robles Sánchez <alum.jrobless.2021@iesalixar.org>
2021-09-22 17:29:53 +0200

    Adios mundo

--- src/main/java/Tema1/T1/Hola_mundo.java
+++ src/main/java/Tema1/T1/Hola_mundo.java
@@ -4,4 +4,8 @@ public class Hola_mundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
     }
+
+    public static void main(String[] args) {
+        System.out.println("Adios mundo");
+    }
 }
