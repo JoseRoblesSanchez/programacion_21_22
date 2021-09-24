d5902c6dd6a8f50cd2a15534fbc0dd4a72dfde31
José Robles Sánchez <alum.jrobless.2021@gmail.com>
2021-09-24 19:34:26 +0200

    Tarea2 - Variables y operaciones básicas

---
+++ Ejercicio2_1.java
@@ -0,0 +1,9 @@
+package Tema1.T1;
+
+    public class Ejercicio2_1<sout> {
+        public static void main(String[] args)
+        {
+            System.out.println(10 * 2);
+        }
+
+}
---
+++ Ejercicio2_5.java
@@ -0,0 +1,14 @@
+package Tema1.T1;
+
+import java.util.Scanner;
+
+public class Ejercicio2_5 {
+    public static void main(String[] args) {
+       // int grado
+        Scanner  teclado = new Scanner(System.in);
+        System.out.println("Dime una temperatura en grados centigrados: ");
+        int grado = teclado.nextInt();
+        System.out.println("Este es el resultado en Fahrenheit = " + 9 * grado / 5 + 32);
+
+    }
+}
